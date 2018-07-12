7c64af455e3513b21374b3d0dcaa8df5f4ec1298
justinpolchinski <justinpolchinski@gmail.com>
2018-07-11 20:47:35 -0400

    changed beta string

--- client/src/pages/singleStock/beta.js
+++ client/src/pages/singleStock/beta.js
@@ -1,4 +1,4 @@
-const beta = "Beta above 1.0 is consider more volatile than the Market. Example: Beta=1.25 is 25% more volatile than the Market."
+const beta = "Beta above 1.0 is considered more volatile than the Market. Example: Beta=1.25 is 25% more volatile than the Market."
 
 
 export default beta;
