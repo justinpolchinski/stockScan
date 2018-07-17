18e04ec2f4272549334642a8140b6182a68f0697
justinpolchinski <justinpolchinski@gmail.com>
2018-07-17 17:18:17 -0400

    changed peg string for tool tip

--- client/src/pages/singleStock/pegRatio.js
+++ client/src/pages/singleStock/pegRatio.js
@@ -1,5 +1,5 @@
 
-const pegRatio = "PEG Ratio: Generally less than 1 is good. Below 2 & greater than 1 is alright. Above 2 or negative is bad."
+const pegRatio = "PEG Ratio: Generally less than 1 is good. Below 2 & greater than 1 is alright. Above 2 or negative is risky and/or bad."
 
 
 export default pegRatio;
